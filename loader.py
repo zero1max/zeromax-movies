f0f867474fb2d20952047c88cbbfa3d3140a54be
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2025-05-03 23:05:50 +0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -2,7 +2,7 @@ from aiogram import Dispatcher , Router, Bot
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 
-TOKEN = "6450010593:AAF2x15EoyvML4mLFnikZkBQ6uB4BgSW9Dw"
+TOKEN = "bot_token"
 
 dp = Dispatcher()
 router_admin = Router()
